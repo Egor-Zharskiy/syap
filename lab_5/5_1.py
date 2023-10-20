04e82d9565d1f0f4d12a51e7d29ef2a0da81fdad
Egor-Zharskiy <egor.zharskiy@mail.ru>
2023-10-20 22:34:29 +0300

    lab 5 task 1

---
+++ lab_5/5_1.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+while True:
+    try:
+        x = float(input("Введите x: "))
+        break
+    except ValueError:
+        print("Неверный ввод")
+
+y = (np.sin(np.pi / 2 + 1) ** 2 + x * np.power(3 + x ** 2, 1 / 4) - np.tan(x ** 3 - 1) ** 3) / (
+        np.arctan(x / 2) - np.log(17.56))
+
+print(y)
